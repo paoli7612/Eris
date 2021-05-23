2659b39456dcd60ef27eda61d1064905b41b5ba7
paoli7612 <paoli7612@gmail.com>
2021-05-23 14:20:45 +0200

    create corsi professori and fill primo semestre

--- database/create.sql
+++ database/create.sql
@@ -1,28 +1,32 @@
-DROP DATABASE IF EXISTS erris;
-CREATE DATABASE erris;
+DROP DATABASE IF EXISTS eris;
+CREATE DATABASE eris;
 
-USE erris;
+USE eris;
 
 CREATE TABLE `professori` (
   `id` int(15) NOT NULL AUTO_INCREMENT,
   `nome` varchar(20) NOT NULL,
   `cognome` varchar(30) NOT NULL,
-  PRIMARY KEY (`id`),
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB;
 
-CREATE TABLE `materie` (
+CREATE TABLE `corsi` (
   `id` int(15) NOT NULL AUTO_INCREMENT,
-  `titolo` varchar(20) NOT NULL,
+  `titolo` varchar(32) NOT NULL,
   `anno` int(4) NOT NULL,
   `idProfessore` int(15) NOT NULL,
   PRIMARY KEY (`id`),
   FOREIGN KEY (`idProfessore`)
     REFERENCES `professori` (`id`)
-    ON DELETE CASCADE,
 ) ENGINE=InnoDB;
 
 
 INSERT INTO `professori` (`id`,`nome`,`cognome`) VALUES
     (1,'Michela', 'Eleuteri'),
     (2,'Arrigo', 'Bonisoli'),
-    (3,'Lingua e letteratura italiana');
+    (3,'Paolo', 'Valente');
+
+INSERT INTO `corsi` (`titolo`,`anno`,`idProfessore`) VALUES
+    ('Analisi matematica 1', 1, 1),
+    ('Algebra lineare', 1, 2),
+    ('Programmazione 1', 1, 3);
