50060b155dd22b9bc9843d7dc2d41e0388fd9cb4
paoli7612 <paoli7612@gmail.com>
2021-05-23 19:23:08 +0200

    all view

--- views/corsi/all.view.php
+++ views/corsi/all.view.php
@@ -2,9 +2,21 @@
     <?php partial('head') ?>
     <body>
         <?php partial('bar') ?>
-
-        <?php foreach ($corsi as $corso): ?>
-            <?= $corso->titolo ?>
-        <?php endforeach; ?>
+        <div class="container">
+            <div class="row">
+                <?php foreach ($corsi as $corso): ?>
+                    <div class="col p-3">
+                        <div class="card">
+                            <div class="card-header">
+                                <?= $corso->titolo ?>
+                            </div>
+                            <div class="card-body">
+                                <?= $corso->idProfessore ?>
+                            </div>
+                        </div>
+                    </div>
+                <?php endforeach; ?>
+            </div>
+        </div>
     </body>
 </html>
