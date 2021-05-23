fc46350b255486414211894862d0d30e33c61b5c
paoli7612 <paoli7612@gmail.com>
2021-05-23 17:02:22 +0200

    pdo excetion error

--- index.php
+++ index.php
@@ -11,5 +11,14 @@
 
     $corsi = array();
 
-    $pdo = new PDO("mysql:host=127.0.0.1;dbname=eris", 'paoli7612', '7612');
+
+    try {
+        $pdo = new PDO("mysql:host=127.0.0.1;dbname=eris", 'paoli7612', '7612');
+    } catch (PDOException $e) {
+        var_dump($e);
+    }
+
+
+
+
     include "index.view.php";
