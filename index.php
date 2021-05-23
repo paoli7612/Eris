3fbc5c67ec207372c2de1c521c3a053f7bd36b9a
paoli7612 <paoli7612@gmail.com>
2021-05-23 17:05:59 +0200

    fetch all corsi

--- index.php
+++ index.php
@@ -15,8 +15,14 @@
     try {
         $pdo = new PDO("mysql:host=127.0.0.1;dbname=eris", 'paoli7612', '7612');
     } catch (PDOException $e) {
-        var_dump($e);
+        die('Connesione con il database fallita');
     }
+
+    $statement = $pdo->prepare("SELECT * FROM corsi;");
+
+    $statement->execute();
+
+    print_r($statement->fetchAll(PDO::FETCH_ASSOC));
 
 
 
