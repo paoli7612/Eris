4786887f9009dcfedef2d6c12e988402bee9f889
paoli7612 <paoli7612@gmail.com>
2021-05-25 18:37:13 +0200

    composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'c4830a3b8126f0ac2555ec08884659f8c70aba33',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'c4830a3b8126f0ac2555ec08884659f8c70aba33',
+    ),
+  ),
+);
