140db8e7e7c29242fb406138c806474a1167b667
paoli7612 <paoli7612@gmail.com>
2021-05-25 18:16:11 +0200

    voto per ogni corso

--- sql/database.sql
+++ sql/database.sql
@@ -5,38 +5,39 @@ CREATE DATABASE eris;
 
 USE eris;
 CREATE TABLE `professori` (
-  `id` int(15) NOT NULL AUTO_INCREMENT,
+  `id` int(8) NOT NULL AUTO_INCREMENT,
   `nome` varchar(150) NOT NULL,
   `cognome` varchar(150) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB;
 
 CREATE TABLE `semestri` (
-  `id` int(15) NOT NULL AUTO_INCREMENT,
+  `id` int(8) NOT NULL AUTO_INCREMENT,
   `parte` int(1) NOT NULL,
   `anno` varchar(8) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB;
 
 CREATE TABLE `corsi` (
-  `id` int(15) NOT NULL AUTO_INCREMENT,
+  `id` int(8) NOT NULL AUTO_INCREMENT,
   `titolo` varchar(100) NOT NULL,
+  `voto` int(2), -- se NULL non ancora passato
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB;
 
 CREATE TABLE `argomenti` (
-  `id` int(15) NOT NULL AUTO_INCREMENT,
+  `id` int(8) NOT NULL AUTO_INCREMENT,
   `path` varchar(100) NOT NULL,
-  `idCorso` int(15) NOT NULL,
+  `idCorso` int(8) NOT NULL,
   PRIMARY KEY (`id`),
   FOREIGN KEY (`idCorso`)
     REFERENCES `corsi` (`id`)
 ) ENGINE=InnoDB;
 
 CREATE TABLE `insegna` (
-  `id` int(15) NOT NULL AUTO_INCREMENT,
-  `idProfessore` int(15) NOT NULL,
-  `idCorso` int(15) NOT NULL,
+  `id` int(8) NOT NULL AUTO_INCREMENT,
+  `idProfessore` int(8) NOT NULL,
+  `idCorso` int(8) NOT NULL,
   PRIMARY KEY (`id`),
   FOREIGN KEY (`idCorso`)
     REFERENCES `corsi` (`id`),
@@ -45,9 +46,9 @@ CREATE TABLE `insegna` (
 ) ENGINE=InnoDB;
 
 CREATE TABLE `frequentato` (
-  `id` int(15) NOT NULL AUTO_INCREMENT,
-  `idCorso` int(15) NOT NULL,
-  `idSemestre` int(15) NOT NULL,
+  `id` int(8) NOT NULL AUTO_INCREMENT,
+  `idCorso` int(8) NOT NULL,
+  `idSemestre` int(8) NOT NULL,
   PRIMARY KEY (`id`),
   FOREIGN KEY (`idCorso`)
     REFERENCES `corsi` (`id`),
