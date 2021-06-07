73ee9a02b581910e2e233750900b24acfe498540
paoli7612 <paoli7612@gmail.com>
2021-06-07 16:34:23 +0200

    form create new course

--- resources/views/courses/index.blade.php
+++ resources/views/courses/index.blade.php
@@ -5,16 +5,65 @@
 @section('content')
     <h1>Corsi</h1>
 
-    <ul>
+
     
-        @foreach ($courses as $course)
-            <li>
-                <a href="{{ $course->route() }}">
-                    {{ $course->title }}
-                </a>
-            </li>
-        @endforeach
-        
-    </ul>
-    
+    <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Titolo</th>
+            <th scope="col">Anno</th>
+          </tr>
+        </thead>
+        <tbody>
+            @foreach ($courses as $course)
+                <tr href="{{ $course->route() }}">
+                    <th scope="row">
+                        {{ $course->id }}
+                    </th>
+                    <td>
+                        {{ $course->title }}
+                    </td>
+                    <td>
+                        {{ $course->year }}
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+      </table>
+
+      <div>
+        <form action="#" method="HEAD">
+            @csrf
+            @method('POST')
+            <div class="row">
+                <div class="form-group col-6">
+                    <label for="title">Titolo</label>
+                    <input class="form-control" id="title" type="text" name="title" placeholder="Analisi Matematica">
+                </div>
+                <div class="form-group col">
+                    <label for="year">Anno</label>
+                    <select class="form-control" name="year" id="year">
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                    </select>
+                </div>
+                <div class="form-group col">
+                    <label for="semester">Semestre</label>
+                    <select class="form-control" name="semester" id="semester">
+                        <option value="1">primo</option>
+                        <option value="2">secondo</option>
+                    </select>
+                </div>
+                <div class="form-group col">
+                    <label for="semester">s</label>
+                    <input class="form-control" type="submit" value="Crea">
+                </div>
+            </div>
+
+            
+        </form>
+    </div>
+
 @endsection
