f6e3f4c586b62be13bdb0cf0d1aad948224126ed
paoli7612 <paoli7612@gmail.com>
2021-06-12 18:08:05 +0200

    restore all

---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+@extends ('layout.app')
+
+@section('content')
+
+    <h1>Home</h1>
+
+@endsection
---
+++ resources/views/layout/navbar.blade.php
@@ -0,0 +1,5 @@
+<div class="container my-3">
+    <a href="/home" class="btn btn-primary">Home</a>
+    <a href="/teachers" class="btn btn-primary">Teachers</a>
+    <a href="/courses" class="btn btn-primary">Courses</a>
+</div>
