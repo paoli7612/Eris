eb566fefe2f8dfccc749c426a6a4f909192f23e2
paoli7612 <paoli7612@gmail.com>
2021-06-12 18:07:46 +0200

    remove all

--- README.md
+++
@@ -1,2 +0,0 @@
-## ERIS
-<img src="doc/img/Schermata del 2021-06-07 00-22-00.png">
--- resources/views/home.blade.php
+++
@@ -1,7 +0,0 @@
-@extends ('layout.app')
-
-@section('title', 'Home')
-
-@section('content')
-    <h1>Home</h1>
-@endsection
