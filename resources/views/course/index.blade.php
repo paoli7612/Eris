3657e4add3279267f150faab230b0fbc0a0bb088
paoli7612 <paoli7612@gmail.com>
2021-06-12 18:42:27 +0200

    course view

--- resources/views/course/index.blade.php
+++ resources/views/course/index.blade.php
@@ -1,7 +1,25 @@
 @extends ('layout.app')
 
-@section('content')
+@section('title', 'Corsi')
 
+@section('content')
     <h1>Courses</h1>
 
+
+    @include('course.new')
+
+    <ul>
+
+        @foreach ($courses as $course)
+
+            <li>
+                <a href="{{ $course->route() }}">
+                    {{ $course->title }}
+                </a>
+            </li>
+
+        @endforeach
+
+    </ul>
+    
 @endsection
