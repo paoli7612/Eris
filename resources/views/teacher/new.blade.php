b5f16b301bef1f076722d9b513d13c1e3e7e6b8f
paoli7612 <paoli7612@gmail.com>
2021-06-12 20:15:56 +0200

    autofocus create new teacher

--- resources/views/teacher/new.blade.php
+++ resources/views/teacher/new.blade.php
@@ -3,8 +3,8 @@
     @method('PUT')
     <div class="container border p-3 bg-dark rounded">
         <div class="form-group row">
-            <input name="name" type="text" class="form-control col mx-3" placeholder="Nome">
-            <input name="surname" type="text" class="form-control col mx-3" placeholder="Cognome">
+            <input name="surname" type="text" class="form-control col mx-3" placeholder="Cognome" autofocus required>
+            <input name="name" type="text" class="form-control col mx-3" placeholder="Nome" required>
             <button class="btn btn-success col mx-3" type="submit">Crea</button>
         </div>
     </div>
