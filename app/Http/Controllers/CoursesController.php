e80816a0b6684f421159633a1e7cccfd3a8d30a9
paoli7612 <paoli7612@gmail.com>
2021-06-12 18:42:19 +0200

    course 1

--- app/Http/Controllers/CoursesController.php
+++ app/Http/Controllers/CoursesController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Course;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class CoursesController extends Controller
 {
@@ -13,6 +14,15 @@ class CoursesController extends Controller
         return view('course.index', [
             'courses' => Course::all()
         ]);
+    }
+
+    public function store(Request $request)
+    {
+        $c = new Course;
+        $c->title = $request->title;
+        $c->slug = Str::slug($c->title);
+        $c->save();
+        return redirect()->back();
     }
 
     public function details(Request $request)
--- database/migrations/2021_06_12_160007_create_courses_table.php
+++ database/migrations/2021_06_12_160007_create_courses_table.php
@@ -15,6 +15,8 @@ class CreateCoursesTable extends Migration
     {
         Schema::create('courses', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('slug')->unique();
             $table->timestamps();
         });
     }
