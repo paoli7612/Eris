fd786d802af16bd2c4a3d95b4788a2235a5400bd
paoli7612 <paoli7612@gmail.com>
2021-06-13 09:02:13 +0200

    autofocus recreate new course

--- resources/views/course/new.blade.php
+++ resources/views/course/new.blade.php
@@ -1,11 +1,12 @@
 <form action="/courses" method="POST">
     @csrf
     @method('PUT')
+
     <div class="container border p-3 bg-dark rounded">
         <div class="form-group row">
-            <input name="title" type="text" class="form-control col mx-3" placeholder="Titolo">
+            <input name="title" type="text" class="form-control col mx-3" placeholder="Titolo" autofocus required>
             <button class="btn btn-success col mx-3" type="submit">Crea</button>
         </div>
     </div>
-
+    
 </form>
