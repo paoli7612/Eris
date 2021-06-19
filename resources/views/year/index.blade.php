57a31394b1f925b9df5ff6328377ae37e46639a1
paoli7612 <paoli7612@gmail.com>
2021-06-19 10:37:10 +0200

    indent

--- resources/views/year/index.blade.php
+++ resources/views/year/index.blade.php
@@ -6,18 +6,15 @@
     <h1>Years</h1>
 
     <ul>
-
         @foreach ($years as $year)
-
             <li>
                 <a href="{{ $year->route() }}">
                     {{ $year->slug }}
                 </a>
             </li>
-
         @endforeach
-
     </ul>
+    
     @if (!App\Models\Year::first())
         <a href="years/init">
             <button class="btn btn-primary">init</button>        
