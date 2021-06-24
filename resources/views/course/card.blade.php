8427cab423661a770f0be81aedb7031faac51d2c
paoli7612 <paoli7612@gmail.com>
2021-06-24 22:04:35 +0200

    null teacher course

--- resources/views/course/card.blade.php
+++ resources/views/course/card.blade.php
@@ -1,8 +1,11 @@
-<div class="btn-success card m-3" onclick="window.location='{{$course->route()}}'">
+<div class="card m-3" onclick="window.location='{{$course->route()}}'">
     <div class="card-header">
         {{ $course->title }}
+        <span> {{ $course->year_id }} </span>
     </div>
     <div class="card-body">
-        <a href="{{ $course->teacher->route() }}">{{ $course->teacher->name }}</a>        
+        @if ($course->teacher)
+            <a href="{{ $course->teacher->route() }}">{{ $course->teacher->name }}</a>                    
+        @endif
     </div>
 </div>
