96f869137185d69d56826c05636e85c64033ae37
paoli7612 <paoli7612@gmail.com>
2021-06-25 22:30:53 +0200

    teete

---
+++ resources/views/_publish-tweet-panel.blade.php
@@ -0,0 +1,10 @@
+    <form action="">
+        <textarea class="m-1 form-control" name="body" placeholder="what do you think? ...">  </textarea>
+
+        <hr>
+        <footer>
+            <img src="https://i.pravatar.cc/40" alt="" class="rounded-circle">
+            <button class="btn btn-primary float-right" type="submit">Tweet</button>
+        </footer>
+        
+    </form>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,12 +1,23 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="d-flex">
-        <div class="mx-4">
+    <div class="row">
+        <div class="col-2">
             @include('_sidebar-links')
         </div>
-        <div class="p-2 flex-fill">2</div>
-        <div class="mx-4">
+        <div class="col">
+            <div class="border border-primary rounded p-3">
+               @include('_publish-tweet-panel')
+            </div>
+
+            <div class="mt-3 border border-secondary rounded p-3">
+                @include ('_tweet')
+                @include ('_tweet')
+                @include ('_tweet')
+                @include ('_tweet')
+            </div>
+        </div>
+        <div class="col-3">
             @include('_friends-list')
         </div>
     </div>
