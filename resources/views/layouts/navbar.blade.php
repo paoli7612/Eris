0eecac9c2f88bb0ef91733c7fb896cb4e34b362d
paoli7612 <paoli7612@gmail.com>
2021-07-03 22:45:19 +0200

    divider in account dropdown auth->check

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,11 +1,4 @@
 <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-    <a class="navbar-brand" href="{{ url('/') }}">
-        {{ config('app.name', 'Laravel') }}
-    </a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
             <li class="nav-item">
@@ -41,7 +34,8 @@
                         <a class="dropdown-item" href="{{ route('settings') }}">
                             {{ __('Settings') }}
                         </a>
-                        
+                        <div class="dropdown-divider"></div>
+
                         <a class="dropdown-item" href="{{ route('logout') }}"
                             onclick="event.preventDefault();
                                             document.getElementById('logout-form').submit();">
