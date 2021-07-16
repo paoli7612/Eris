770d26b01f34e1b1c83014a07b6089eecc6959e7
paoli7612 <paoli7612@gmail.com>
2021-07-16 21:43:09 +0200

    admin index panel

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,6 +18,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('admin', [AdminController::class, 'index']);
 
 Auth::routes();
 
