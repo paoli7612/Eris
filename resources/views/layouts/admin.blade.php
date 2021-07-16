39b508e12c4ae6745e8c679d5c6ccf1086f7cbc8
paoli7612 <paoli7612@gmail.com>
2021-07-16 21:37:19 +0200

    site and admin panel

---
+++ resources/views/layouts/admin.blade.php
@@ -0,0 +1,12 @@
+<html>
+
+    <head>
+        <title>Eris - Admin</title>
+        <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    </head>
+
+    <body>
+        @yield('content')
+    </body>
+
+</html>
