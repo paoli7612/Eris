15a8016f91d43304a8a055c8b1a3553b1de38f71
paoli7612 <paoli7612@gmail.com>
2021-07-18 17:58:42 +0200

    set up page home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,17 @@
 @extends('layouts.app')
 
 @section('content')
-    
+    <div class="jumbotron mt-5">
+        <div class="container">
+            <h1 class="display-3">Bentornato, {{ Auth::user()->name }}</h1>
+            <p>Grazie per aver scelto Apprendiamoci.</p>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="row">
+            <h1>Home</h1>
+        </div>
+    </div>
+
 @endsection
