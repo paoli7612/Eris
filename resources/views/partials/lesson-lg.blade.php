59077383e01c87139d6f9acc8b1e1b56ab7aee56
paoli7612 <paoli7612@gmail.com>
2021-07-19 06:58:18 +0200

    delete views

--- resources/views/partials/lesson-lg.blade.php
+++ resources/views/partials/lesson-lg.blade.php
@@ -40,12 +40,51 @@
                     <button class="btn bg-white mb-3" disabled>
                         <i class="fas fa-upload"></i>
                         Carica materiali</button>
-                    <a class="btn bg-white mb-3" href="elimina">
+                    <a class="btn bg-white mb-3" href="/lezioni/{{ $lesson->id }}/elimina">
                         <i class="fas fa-trash"></i>
                         Elimina</a>
                     <?php endif; ?>
                 </div>
             </div>
         </div>
+
+
+
     </div>
+
+    <div class="row mt-3">
+        @for ($i = 0; $i < 6; $i++)
+            <div class="col-xl-6 col-md-12 col-sm-12 mb-3">
+                <button class="btn btn-block bg-primary w-100 p-3" style="height: 100%"
+                    onclick="window.location='../materiali/?id={{ $lesson->id }}&tipo=1'">
+                    <div class="row">
+                        <div class="col-8">
+                            <h4 class="float-left">
+                            </h4>
+                        </div>
+                        <h1 class="col-4">
+                            <i class="fas fa-book float-right"></i>
+                        </h1>
+                    </div>
+                </button>
+            </div>
+        @endfor
+     
+        <div class="container">
+            <div class="card bg-light card-body">
+                <div class="row">
+                    <div class="col">
+                        <h3>Nessun materiale caricato</h3>
+                        <p>Il docente non ha ancora caricato materiali in questa lezione</p>
+                    </div>
+                    <div class="col">
+                        <h1 class="float-right">
+                            <i class="fas fa-exclamation-triangle"></i>
+                        </h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
 </div>
---
+++ resources/views/partials/lesson-md.blade.php
@@ -0,0 +1,35 @@
+<div class="container-fluid">
+    <ol class="breadcrumb">
+        <li class="breadcrumb-item">
+            <a href="../home/">Home</a>
+        </li>
+        <li class="breadcrumb-item">
+            <a href="../lezioni/">Lezioni</a>
+        </li>
+        <li class="breadcrumb-item">
+            <a href="{{ route('lezione', $lesson->id) }}">{{ $lesson->title }}</a>
+        </li>
+        <li class="breadcrumb-item">
+            Elimina
+        </li>
+    </ol>
+
+    <div>
+        <form action="{{ route('lezione', $lesson->id) }}" method="post">
+            @csrf
+            @method('delete')
+
+            <p> Sei sicuro di voler eliminare la lezione <b>{{$lesson->title}}</b>? </p>
+
+            <a href="." class="btn btn-secondary">
+                Annnulla
+            </a>
+
+            <button type="submit" class="btn btn-danger">
+                <i class="fas fa-trash"></i>
+                Elimina lezione
+            </button>
+
+        </form>
+    </div>
+</div>
