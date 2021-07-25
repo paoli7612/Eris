ce8e49b3d4eaf175bd5fec8b746cb22d5b4cb083
paoli7612 <paoli7612@gmail.com>
2021-07-25 15:12:54 +0200

    route key name nickname of user

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,6 +18,8 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
+        'surname',
+        'nickname',
         'email',
         'password',
     ];
@@ -40,4 +42,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function getRouteKeyName()
+    {
+        return 'nickname';
+    }
 }
