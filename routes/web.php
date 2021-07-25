41fe2bd1a8f25829fed6f941b2b40521bff1a3be
paoli7612 <paoli7612@gmail.com>
2021-07-25 14:33:46 +0200

    first 3 pages

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,4 +21,6 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+Route::get('/teachers', [HomeController::class, 'index'])->name('teachers');
+Route::get('/corsi', [HomeController::class, 'index'])->name('courses');
