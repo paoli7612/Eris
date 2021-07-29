87b585d7b9d64f31dbc6ff6265dd306870711c6b
paoli7612 <paoli7612@gmail.com>
2021-07-29 20:52:22 +0200

    si

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -14,6 +14,7 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
+        User::factory()->count(10)->create(['type' => 'teacher']);
         User::factory()->count(10)->create();
     }
 }
