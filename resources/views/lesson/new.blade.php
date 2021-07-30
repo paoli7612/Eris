644daec50056ac6708225cbb2ec923e032f8c2d6
paoli7612 <paoli7612@gmail.com>
2021-07-31 00:48:24 +0200

    form new lesson

---
+++ resources/views/lesson/new.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="card">
+        <div class="card-header">
+            {{ __('New lesson') }}
+        </div>
+        <div class="card-body">
+            <form action="{{ route('new-lesson') }}" method="post">
+                @csrf
+                <div class="form-group">
+                    <div class="row">
+                        <div class="col">
+                            <input type="text" name="title" class="form-control" placeholder="title">
+                        </div>
+                        <div class="col text-right">
+                            <button type="submit" class="btn btn-primary w-100">
+                                Create lesson
+                                <i class="fas fa-plus"></i>
+                            </button>
+                        </div>
+                    </div>
+                    
+                </div>
+            </form>            
+        </div>
+    </div>
+@endsection
