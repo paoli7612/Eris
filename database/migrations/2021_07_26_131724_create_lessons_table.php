3329a2e5dc86ee9dc4951c4aea49390c9c61a333
paoli7612 <paoli7612@gmail.com>
2021-07-31 02:01:24 +0200

    add description

--- database/migrations/2021_07_26_131724_create_lessons_table.php
+++ database/migrations/2021_07_26_131724_create_lessons_table.php
@@ -18,6 +18,7 @@ class CreateLessonsTable extends Migration
             $table->id();
             $table->string('title')->unique();
             $table->string('slug')->unique();
+            $table->text('description')->nullable();
             $table->foreignIdFor(Course::class, 'course_id');
             $table->timestamps();
         });
