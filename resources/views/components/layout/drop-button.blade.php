3d3866f161e7aac04a93f2bde01101a07202b1e4
Tommaso <tommaso.paoli@zanottienergygroup.it>
2021-08-24 09:02:03 +0200

    search and new lesson in section hide

--- resources/views/components/layout/drop-button.blade.php
+++ resources/views/components/layout/drop-button.blade.php
@@ -1,4 +1,4 @@
-<button class="btn btn-{{ $color }} shadow mb-3" type="button" data-toggle="collapse" data-target="#{{ $id }}"
+<button class="btn btn-info text-white shadow mb-3" type="button" data-toggle="collapse" data-target="#{{ $id }}"
     aria-expanded="false" aria-controls="collapseExample">
     <i class="{{ $icon }}"></i>
     {{ $title }}
--- resources/views/lesson/index.blade.php
+++ resources/views/lesson/index.blade.php
@@ -11,10 +11,53 @@
 
     @auth
         @if (auth()->user()->type == 'teacher')
-            <x-layout.drop-button id="collapseExample" icon="fa fa-search" title="{{ __('Search...') }}" color="info"/>
-            <x-layout.tools-item link="{{ route('lesson.new') }}" icon="fa fa-plus" title="{{ __('New lesson') }}" color="info" />
-
-            <x-layout.drop-div id="collapseExample" />
+            <x-layout.drop-button id="collapseSearch" icon="fa fa-search" title="{{ __('Search...') }}" />
+            <x-layout.drop-button id="collapseNew" icon="fa fa-plus" title="{{ __('New lesson') }}" />
+            <x-layout.drop-div id="collapseSearch">
+                <form method="GET" class="my-3">
+                    <div class="input-group col-6 mx-auto">
+                        <input placeholder="{{ __('search..') }}" name="search" type="search" class="form-control"
+                            value="{{ request('search') ?? '' }}" />
+                        <button type="button" class="btn btn-primary">
+                            <i class="fa fa-search"></i>
+                        </button>
+                    </div>
+                </form>
+            </x-layout.drop-div>
+            <x-layout.drop-div id="collapseNew">
+                <div class="row">
+                    <div class="col-12 col-sm-6 mx-auto">
+                        <div class="card">
+                            <div class="card-header">
+                                {{ __('New lesson') }}
+                            </div>
+                            <div class="card-body">
+                                <form action="{{ route('lesson.new') }}" method="POST">
+                                    @csrf
+                                    <div class="form-group">
+                                        <label for="title">{{ __('Title') }}</label>
+                                        <input type="text" name="title" placeholder="title" class="form-control">
+                                    </div>
+                                    <div class="form-group">
+                                        <input type="text" name="teacher" class="form-control" value="{{ auth()->user()->complete_name }}" disabled>
+                                    </div>
+                                    <div class="form-group">
+                                        <select name="course_id" class="form-control">
+                                            @foreach (App\Models\Course::all() as $course)
+                                                <option value="{{ $course->id }}">{{ $course->name }}</option>
+                                            @endforeach  
+                                        </select>
+                                    </div>
+                                    <div class="form-group text-right">
+                                        <a class="btn btn-danger" href="{{ route('lessons') }}"> {{ __('Back')}} </a>
+                                        <input type="submit" value="{{ __('New lesson') }}" class="btn btn-success">
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </x-layout.drop-div>
 
         @endif
     @endauth
