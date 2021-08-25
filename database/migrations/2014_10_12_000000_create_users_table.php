05ff56c3a908cbc574404f5b7ea6bf23bdb3faac
paoli7612 <tomaoli7612@gmail.com>
2021-08-25 11:09:42 +0200

    background image

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -22,6 +22,7 @@ class CreateUsersTable extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('avatar')->nullable();
+            $table->string('matricola', 6)->nullable(); // 152542
             $table->rememberToken();
             $table->timestamps();
         });
--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -2,4 +2,9 @@
     <title>Eris - @yield('title')</title>
     <link type="text/css" rel="stylesheet" href="{{ mix('css/app.css') }}">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <style>
+        body {
+            background-image: url( {{ asset('back.png') }} )
+        }
+    </style>
 </head>
