fb0015f6128d234466b0507f127cb512f9e70b3e
paoli7612 <paoli7612@gmail.com>
2021-08-26 02:15:38 +0200

    lessons of every show teachers

--- resources/views/user/show.blade.php
+++ resources/views/user/show.blade.php
@@ -24,4 +24,30 @@
             </div>
         </div>
     </div>
+    <div class="row">
+        <div class="mt-3 col">
+            @if (count($user->lessons))
+                <div class="bg-light p-3 shadow">
+                    <div class="text-center">
+                        <h1>
+                            {{ __('Lessons') }}    
+                        </h1>
+                    </div>                    
+    
+                    <div class="row">
+                        @foreach ($user->lessons as $lesson)
+                            <x-lesson :lesson="$lesson" col="12" />
+                        @endforeach
+                    </div>
+    
+                </div>
+            @else
+                <div class="jumbotron m-5 shadow">
+                    <h1>No lessons created</h1>
+                    <p class="lead">You didn't create neither a lesson.</p>
+                    <a class="btn btn-lg btn-primary" href="{{ route('lessons') }}?new" role="button">Create now</a>
+                </div>
+            @endif
+        </div>    
+    </div>
 @endsection
