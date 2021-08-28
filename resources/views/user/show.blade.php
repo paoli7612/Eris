c70b785da49d9f0f8cce99885d47c4d20eb701ab
paoli7612 <paoli7612@gmail.com>
2021-08-28 08:22:12 +0200

    in partials

--- resources/views/user/show.blade.php
+++ resources/views/user/show.blade.php
@@ -7,6 +7,7 @@
     <x-tt name="account" text="normal">
         {{ $user->type_ucf }}
     </x-tt>
+
     <x-bc>
         <x-bc-item link="{{ route('home') }}">Home</x-bc-item>
         <x-bc-item link="{{ route('teachers') }}">Teachers</x-bc-item>
@@ -44,35 +45,8 @@
             </div>
         </div>
     </div>
-    <div class="mt-3">
-        @if ($user->isTeacher())
-            @if (count($user->lessons))
-                <div class="bg-light p-3 shadow">
-                    <div class="text-center">
-                        <h1>
-                            {{ __('Lessons') }}
-                        </h1>
-                    </div>
-                    <div class="row">
-                        @foreach ($user->lessons as $lesson)
-                            <x-lesson :lesson="$lesson" col="12" />
-                        @endforeach
-                    </div>
-                </div>
-            @else
-                <div class="jumbotron m-5 shadow">
-                    <h1>No lessons created</h1>
-                    @if ($user->isMe())
-                        <p class="lead">You didn't created any lessons</p>
-                        <a href="{{ route('lessons?new') }}">
-                            Let's create one
-                        </a>
-                    @else
-                        <p class="lead">This account didn't created any lessons</p>
-                    @endif
-                </div>
-            @endif
-        @endif
+    @if ($user->isTeacher())
+        @include ('partials.my-lessons', $user)
+    @endif
 
-    </div>
 @endsection
