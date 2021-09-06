f3bca7b2f018eacb20593a0fa05fe91ac1ddaccf
paoli7612 <tomaoli7612@gmail.com>
2021-09-06 17:45:16 +0200

    cosi

--- resources/views/user/show.blade.php
+++ resources/views/user/show.blade.php
@@ -19,13 +19,66 @@
             <x-dd-button id="collapseSettings" title="{{ __('Settings') }}" />
             <x-dd-button id="collapseLogout" title="{{ __('Logout') }}" />
             <x-dd-div id="collapseSettings">
-                @include('forms.user-setting')
+                <form action="{{ route('account.settings') }}" method="POST" enctype="multipart/form-data">
+                    @csrf
+                    @method('put')
+                
+                    <div class="mx-auto col-6 bg-light p-3 shadow">
+                        <div class="row">
+                            <div class="col">
+                                <div class="form-group">
+                                    <label for="name">Name</label>
+                                    <input type="text" name="name" class="form-control @error('name') is-invalid @enderror"
+                                        id="name" value="{{ $user->name }}">
+                                </div>
+                                <div class="form-group">
+                                    <label for="surname">Surname</label>
+                                    <input type="text" name="surname"
+                                        class="form-control @error('surname') is-invalid @enderror" id="name"
+                                        value="{{ $user->surname }}">
+                                </div>
+                                <div class="form-group">
+                                    <label for="email">Email</label>
+                                    <input type="text" name="email" class="form-control" id="email"
+                                        value="{{ $user->email }}" disabled>
+                                </div>
+                            </div>
+                            <div class="col-4 text-center">
+                                <img src="{{ $user->img }}" class="rounded-circle w-100 shadow" alt="avatar">
+                                <button type="button" class="btn btn-info mt-3" onclick="$('#avatar').click()">
+                                    {{ __('Change') }}
+                                </button>
+                                <input id="avatar" name="avatar" type="file" class="form-control mt-3 d-none" onchange="">
+                            </div>
+                        </div>
+                
+                        <div class="row">
+                            <div class="col">
+                                @if ($user->type == 'student')
+                                    <x-dd-button id="collapseRem" title="{{ __('Delete account') }}"
+                                        color="outline-danger" text="normal" />
+                
+                                @endif
+                
+                                <a href="{{ route('teacher', $user) }}" class="btn btn-info text-white">
+                                    <i class="fa fa-return"></i>
+                                    Back
+                                </a>
+                
+                                <button type="submit" class="btn btn-success ">
+                                    <i class="fa fa-save"></i>
+                                    Save
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </form>
             </x-dd-div>
             <x-dd-div id="collapseLogout">
-                <x-q title="logout" ask="Are you sure to logout?" action="{{ route('logout') }}"  method="POST" />
+                <x-q title="logout" collapse="collapseLogout" ask="Are you sure to logout?" action="{{ route('logout') }}"  method="POST" />
             </x-dd-div>
             <x-dd-div id="collapseRem">
-                <x-q title="remove" ask="Are you sure to delete this account?" action="{{ route('account.delete') }}" method="POST"/>
+                <x-q title="remove" collapse="collapseRem" ask="Are you sure to delete this account?" action="{{ route('account.delete') }}" method="POST"/>
             </x-dd-div>
         </div>
     @endif
